5328c260f94941e107d384e796a16b0ae0968088
eminwux <emilianofs@gmail.com>
2024-09-03 17:46:53 +0200

    fix: entrypoint

--- keycloak/volumes/scripts/entrypoint.sh
+++ keycloak/volumes/scripts/entrypoint.sh
@@ -5,6 +5,7 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
+MOUNT=/mnt/keycloak
 # Function to echo in green
 echo_green() {
     echo -e "${GREEN}$1${NC}"
@@ -35,31 +36,21 @@ delete_file(){
 
 check_env_var "KC_HOSTNAME"
 
-if [ ! -f /mnt/keycloak/server.keystore ]; then
+if [ ! -f $MOUNT/server.keystore ]; then
     
-    cd /mnt/keycloak/
 
-    delete_file /mnt/keycloak/server.keystore 
-    delete_file /mnt/keycloak/server.crt 
+    delete_file $MOUNT/server.keystore 
+    delete_file $MOUNT/server.crt 
 
     # Generate key pair for server
     echo_green "Generating key pair..."
-    keytool -genkeypair -storepass password -storetype PKCS12 -keyalg RSA -keysize 2048 -dname "CN=server" -alias server -ext "SAN:c=DNS:localhost,IP:127.0.0.1,DNS:$KC_HOSTNAME" -keystore server.keystore
+    keytool -genkeypair -storepass password -storetype PKCS12 -keyalg RSA -keysize 2048 -dname "CN=server" -alias server -ext "SAN:c=DNS:localhost,IP:127.0.0.1,DNS:$KC_HOSTNAME" -keystore $MOUNT/server.keystore 
 
     ok_or_exit
 
     # Export the public certificate from keystore
     echo_green "Exporting certificate..."
-    keytool -exportcert -alias server -file server.crt -keystore server.keystore -storepass password -rfc
-    ok_or_exit
-
-    # Build Keycloak configurations
-    echo_green "Building Keycloak configuration..."
-    /opt/keycloak/bin/kc.sh build
-    ok_or_exit
-
-    echo_green "Displaying Keycloak configuration..."
-    /opt/keycloak/bin/kc.sh show-config
+    keytool -exportcert -alias server -file $MOUNT/server.crt  -keystore $MOUNT/server.keystore  -storepass password -rfc
     ok_or_exit
 
 fi
@@ -67,10 +58,19 @@ fi
 # Link server.keystore to the Keycloak configuration directory
 delete_file /opt/keycloak/conf/server.keystore
 echo_green "Linking keystore..."
-ln -s `pwd`/server.keystore /opt/keycloak/conf/
+ln -s $MOUNT/server.keystore /opt/keycloak/conf/
+ok_or_exit
+
+# Build Keycloak configurations
+echo_green "Building Keycloak configuration..."
+/opt/keycloak/bin/kc.sh build
+ok_or_exit
+
+echo_green "Displaying Keycloak configuration..."
+/opt/keycloak/bin/kc.sh show-config
 ok_or_exit
 
 # Start Keycloak with development settings and log level info
 echo_green "Starting Keycloak..."
-/opt/keycloak/bin/kc.sh start-dev --log-level=debug
+/opt/keycloak/bin/kc.sh start-dev --log-level=info
 ok_or_exit
